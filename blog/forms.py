7a684e13384ecae2b93e1c6caec880ffa17b5f43
Ryan Bahr <Ry.bahr@gmail.com>
2019-03-26 15:53:17 -0700

    cool

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from blog.models import post
+# class CommentForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = comment
+#         fields = ('text',)
